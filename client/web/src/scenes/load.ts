5b202f3c4d88fe2579156e2c8dc2d289ea54e1af
Sam Szuflita <sam@ridereportapp.com>
2022-05-24 16:54:24 -0400

    Merge develop

--- client/web/src/scenes/load.ts
+++ client/web/src/scenes/load.ts
@@ -1,19 +1,11 @@
 import { InterpolationBuffer } from "interpolation-buffer";
 import { Player, PlayerState } from "../../../../api/types";
-import {
-  HathoraClient,
-  HathoraConnection,
-  UpdateArgs,
-} from "../../../.hathora/client";
+import { HathoraClient, HathoraConnection, UpdateArgs } from "../../../.hathora/client";
 import { VIEWPORT_WIDTH, VIEWPORT_HEIGHT } from "../utils";
 
 export class LoadScene extends Phaser.Scene {
   create() {
-    this.add
-      .text(VIEWPORT_WIDTH / 2, VIEWPORT_HEIGHT / 2, "Loading...", {
-        fontSize: "50px",
-      })
-      .setOrigin(0.5);
+    this.add.text(VIEWPORT_WIDTH / 2, VIEWPORT_HEIGHT / 2, "Loading...", { fontSize: "50px" }).setOrigin(0.5);
 
     const client = new HathoraClient();
     if (sessionStorage.getItem("token") === null) {
@@ -28,39 +20,22 @@ export class LoadScene extends Phaser.Scene {
   }
 }
 
-function start(
-  client: HathoraClient,
-  token: string,
-  scenePlugin: Phaser.Scenes.ScenePlugin
-) {
+function start(client: HathoraClient, token: string, scenePlugin: Phaser.Scenes.ScenePlugin) {
   let connection: HathoraConnection;
   let buffer: InterpolationBuffer<PlayerState>;
   getConnection(client, token, ({ state, updatedAt }) => {
     if (buffer === undefined) {
       buffer = new InterpolationBuffer<PlayerState>(state, 50, lerp);
-      scenePlugin.start("help", {
-        connection,
-        buffer,
-        user: HathoraClient.getUserFromToken(token),
-      });
+      scenePlugin.start("game", { connection, buffer, user: HathoraClient.getUserFromToken(token) });
     } else {
       buffer.enqueue(state, updatedAt);
     }
   }).then((conn) => (connection = conn));
 }
 
-async function getConnection(
-  client: HathoraClient,
-  token: string,
-  onStateChange: (args: UpdateArgs) => void
-) {
+async function getConnection(client: HathoraClient, token: string, onStateChange: (args: UpdateArgs) => void) {
   if (location.pathname.length > 1) {
-    return client.connect(
-      token,
-      location.pathname.split("/").pop()!,
-      onStateChange,
-      console.error
-    );
+    return client.connect(token, location.pathname.split("/").pop()!, onStateChange, console.error);
   } else {
     const stateId = await client.create(token, {});
     history.pushState({}, "", `/${stateId}`);
@@ -68,17 +43,11 @@ async function getConnection(
   }
 }
 
-function lerp(
-  from: PlayerState,
-  to: PlayerState,
-  pctElapsed: number
-): PlayerState {
+function lerp(from: PlayerState, to: PlayerState, pctElapsed: number): PlayerState {
   return {
     players: to.players.map((toPlayer) => {
       const fromPlayer = from.players.find((p) => p.id === toPlayer.id);
-      return fromPlayer !== undefined
-        ? lerpPlayer(fromPlayer, toPlayer, pctElapsed)
-        : toPlayer;
+      return fromPlayer !== undefined ? lerpPlayer(fromPlayer, toPlayer, pctElapsed) : toPlayer;
     }),
     platforms: to.platforms,
   };
